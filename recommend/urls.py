841787353078df26f719e0560daf5d7f96b07cc3
Rayhan <rayhanpyo2016@gmail.com>
2025-04-26 16:12:42 +0800

    Initial commit for Render deployment

--- recommend/urls.py
+++ recommend/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-   
+    path('admin/', admin.site.urls),
+    # Add your app URLs here, for example:
+    # path('', include('recommend.urls')),
 ]
