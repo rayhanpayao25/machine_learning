dbc3a97fd72d6c7d75e4a8c4c7948698d8023d62
Rayhan <rayhanpyo2016@gmail.com>
2025-04-26 14:55:26 +0800

    Initial commit

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -95,10 +95,16 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional if you have a 'static/' folder
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
